deb6ffe5e5d67f75b5aafd07ba6461df56c1f3cb
adamsoja1 <adamek0222@gmail.com>
2023-05-23 21:30:56 +0200

    Changed docker-compose file, changed models, added relations between tables

--- voice_recorder/admin.py
+++ voice_recorder/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Record
+from .models import Record,Author,RecordGroup
 
 admin.site.register(Record)
-
+admin.site.register(RecordGroup)
+admin.site.register(Author)
--- voice_recorder/models.py
+++ voice_recorder/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.utils import timezone
+
+from datetime import datetime
+
+
 class Record(models.Model):
     date = models.DateField(default=timezone.now)
+    time = models.TimeField(default=datetime.now())
+
     audio_file = models.FileField(upload_to='records/')
+    record = models.ForeignKey('RecordGroup',
+                               on_delete=models.CASCADE)
 
+    author = models.ForeignKey('Author',
+                               on_delete=models.CASCADE)
+class RecordGroup(models.Model):
+    date = models.DateField(default=timezone.now)
+    time = models.TimeField(default=datetime.now())
+    def __str__(self):
+        return(str(self.date) + ' ' + str(self.time))
 
-class Test(models.Model):
-    autor = models.CharField(max_length=15)
+class Author(models.Model):
+    name = models.CharField(max_length=30)
 
     def __str__(self):
-        return(autor)
+        return(self.name)
